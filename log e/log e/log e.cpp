bc470648e0927cc92f8f820b19e65f74838b1b1e
G2P2S <maksim.knikhuta.90@mail.ru>
2022-07-12 17:56:11 +0300

    1

---
+++ log e/log e/log e.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+double lnx(double x, double e)
+{
+    double p, rez;
+    int i;
+    x--;
+    p = rez = x;
+    i = 2;
+    while (fabs(p) >= e)
+    {
+        p *= -x;
+        rez += p / i;
+        i++;
+    }
+    return rez;
+}
+int main() {
+    setlocale(LC_ALL, "Ru");
+    long double x, e;
+    cout << "введите точность ";
+    cin >> e;
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    in >> x;
+    if (x == 1) {
+        out << 0 << endl;
+    }
+    if (x < 0 || e < 0) {
+        out << "ОШИБКА" << endl;
+    }
+    if (x < 1 && e > 0)
+    {
+        out << lnx(x, e);
+    }
+    if (x > 1) {
+        x = 1 / x;
+        out << -lnx(x, e) << endl;
+    }
+    return 0;
+}
